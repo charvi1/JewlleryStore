856830de9f52f4fb7d5e38bde7292940b529c49b
charvi1 <charvi236.be22@chitkara.edu.in>
2025-05-20 18:06:35 +0530

    payment gateway+cart+pro

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -5,8 +5,7 @@ const express = require("express");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const router = express.Router();
- // Your Stripe Secret key (test mode)
-
+ 
 // Create a payment intent
 router.post("/create-payment-intent", async (req, res) => {
   try {
